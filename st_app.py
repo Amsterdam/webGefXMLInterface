86240e56821d6d4e001693a5b13ae0ec6e8a6b6d
Thomas van der Linden <t.van.der.linden@amsterdam.nl>
2023-02-14 21:30:17 +0100

    requirements voor flask

---
+++ st_app.py
@@ -0,0 +1,45 @@
+# https://stackoverflow.com/questions/72442371/deploying-streamlit-app-in-azure-without-using-docker
+# https://www.youtube.com/watch?v=2toRzAYT8yo
+# let op! werkt niet met Python 3.9.7
+
+import streamlit as st
+from io import StringIO, BytesIO
+import matplotlib.pyplot as plt
+
+st.title('Visualiseer geotechnisch grondonderzoek')
+
+from gefxml_reader import Cpt
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+
+    # To read file as string:
+    string_data = stringio.read()
+
+    cpt = Cpt()
+    if uploaded_file.name.lower().endswith('xml'):
+        cpt.load_xml(string_data, checkAddDepth=True, checkAddFrictionRatio=True, file=False)
+    elif uploaded_file.name.lower().endswith('gef'):
+        cpt.load_gef(string_data, checkAddDepth=True, checkAddFrictionRatio=True, fromFile=False)
+
+    fig = cpt.plot(returnFig=True)
+
+    fn = uploaded_file.name.replace('.xml', '.png')
+    img = BytesIO()
+    plt.savefig(img, format='png')
+    
+    btn = st.download_button(
+    label="Download image",
+    data=img,
+    file_name=fn,
+    mime="image/png"
+    )
+
+
+
+    st.pyplot(fig)
